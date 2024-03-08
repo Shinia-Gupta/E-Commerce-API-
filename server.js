ff4a62e8d33e51f1830bed394a2bd68ed379fe29
Shinia <113818197+Shinia-Gupta@users.noreply.github.com>
2024-03-08 18:40:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './index.js';
 import { connectDB } from './config/connection.js';
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT,process.env.HOSTNAME,()=>{
     console.log(`server is listening at port ${process.env.PORT}`);
     connectDB();
 })
